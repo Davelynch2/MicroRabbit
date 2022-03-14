8da5231db3c9c08ba05d5e11d9f1345ecb84c5be
David <david.joset@yahoo.fr>
2022-03-13 21:39:42 -0400

    Refactor RabbitMQ Bus for Depednecy Injection for parameterless constructors in our event handler

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -24,7 +24,14 @@ namespace MicroRabbit.Infra.IoC
         public static void RegisterServices(IServiceCollection services)
         {
             //Domain Bus
-            services.AddTransient<IEventBus, RabbitMQBus>();
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            });
+
+            // subscription
+            services.AddTransient<TransferEventHandler>();
 
             // Doamin Events
             services.AddTransient<IEventHandler<TransferCreatedEvent>, TransferEventHandler>();
